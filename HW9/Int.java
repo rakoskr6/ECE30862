4e90cea9df8dfb3e6362ca986f9fdc12da4ca99a
rakoskr6 <rakoskr6@users.noreply.github.com>
2015-09-28 16:13:25 -0400

    Implemented the interface...hopefully

--- HW9/Int.java
+++ HW9/Int.java
@@ -1,12 +1,43 @@
 import java.lang.Integer;
 
-class Int . . . {
+class Int implements Comparable {
 
    Integer i;
 
    public Int(int j) {i = new Integer(j);}
 
    // Implement the Comparable interface functions here.
-   . . . 
+   public double value(){  
+      return 1;
+      
+   }
+  
+   public boolean lessThan(Comparable c){  
+      if (this.value < c.value) {
+         return true;
+      }
+      else {
+         return false;
+      }
+   }
+  
+   public boolean equal(Comparable c){  
+      if (this.value == c.value) {
+         return true;
+      }
+      else {
+         return false;
+      }
+   }
+  
+   public boolean greaterThan(Comparable c){ 
+   if (this.value > c.value) {
+         return true;
+      }
+      else {
+         return false;
+      }
+      
+   }
 
 }
